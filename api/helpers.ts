b03ea3d7873878fcbcf898e5f88045860a5ffea2
IlunhaWirebee <ilunha@wirebee.com>
2024-10-09 23:34:15 +0300

    add post a free job endpoint

--- api/helpers.ts
+++ api/helpers.ts
@@ -1,5 +1,9 @@
 import nodemailer from "nodemailer";
 import { emailTemplateFn } from "./htmlTempates/emailTemplate";
+import FormData from "form-data";
+import axios from "axios";
+import crypto from "crypto";
+import fs from "fs";
 
 export const createTableString = function (data: any): string {
    let tableString = '<table class="GeneratedTable">';
@@ -41,4 +45,69 @@ export const sendEmail = async function (emailBody: string, emailSubject: string
 };
 export function getUrlExtension(url: any) {
    return url.split(/[#?]/)[0].split(".").pop().trim();
+}
+
+// Function to get MD5 hash of a file buffer
+function getFileHash(fileBuffer: any) {
+   return crypto.createHash("md5").update(fileBuffer).digest("hex");
+}
+
+// Function to upload file to Webflow
+export async function uploadFileToWebflow(file: any) {
+   const fileName = file.originalFilename;
+   const filePath = file.filepath;
+
+   const fileBuffer = fs.readFileSync(filePath);
+   const fileHash = getFileHash(fileBuffer);
+
+   // Step 1: Get upload details from Webflow
+   const url = "https://api.webflow.com/v2/sites/6299d639266385b43ef0562f/assets";
+   const uploadOptions = {
+      method: "POST",
+      headers: {
+         accept: "application/json",
+         "content-type": "application/json",
+         authorization: `Bearer ${process.env.WEBHOOK_API_TOKEN}`,
+      },
+      data: {
+         fileName,
+         fileHash,
+      },
+   };
+
+   try {
+      const uploadDetailsResponse = await axios(url, uploadOptions);
+      const uploadDetails = uploadDetailsResponse.data;
+
+      // Step 2: Upload the file to the S3 endpoint
+      const s3Endpoint = uploadDetails.uploadUrl;
+      const form = new FormData();
+
+      // Add form fields
+      for (const key in uploadDetails.uploadDetails) {
+         form.append(key, uploadDetails.uploadDetails[key]);
+      }
+
+      // Append the actual asset file to the FormData object
+      form.append("file", fileBuffer, fileName);
+
+      // Upload the file to S3
+      const s3Response = await axios.post(s3Endpoint, form, {
+         headers: {
+            ...form.getHeaders(),
+         },
+      });
+
+      if (s3Response.status === 201) {
+         console.log("Upload successful.");
+         // Return the file ID from Webflow response
+         return s3Response.headers.location;
+      } else {
+         console.error("Upload failed with status:", s3Response.status);
+         return null;
+      }
+   } catch (error) {
+      console.error("Error uploading file:", error);
+      return null;
+   }
 }
--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { createTableString, getGmailTransporter, getUrlExtension, sendEmail } from "./helpers";
+import {
+   createTableString,
+   getGmailTransporter,
+   getUrlExtension,
+   sendEmail,
+   uploadFileToWebflow,
+} from "./helpers";
 import formidable from "formidable";
 import { applyEmailTemplateFull } from "./htmlTempates/apllyEmailFull";
 import { applyEmailTemplateWithoutLinkedin } from "./htmlTempates/apllyEmailWithoutLinkedin";
+import axios from "axios";
 
 const app = express();
 dotenv.config();
@@ -23,6 +30,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
    res.send("Hello World!");
 });
+const MAX_VERCEL_FILE_SIZE_IN_REQUEST = 4000000;
 
 app.post("/api/contact-form-submission", async (req, res) => {
    console.log("🚀 ~ app.post ~ headers", req.headers);
@@ -41,7 +49,6 @@ app.post("/api/contact-form-submission", async (req, res) => {
 });
 
 app.post("/api/apply-form-submission", async (req, res, next) => {
-   const MAX_VERCEL_FILE_SIZE_IN_REQUEST = 4000000;
    console.log("🚀 ~ app.post ~ headers", req.headers);
    console.log("🚀 ~ app.post ~ user-agent", req.headers["user-agent"]);
 
@@ -111,6 +118,262 @@ app.post("/api/apply-form-submission", async (req, res, next) => {
       } catch (error) {
          console.error("Error sending email", error);
       }
+
+      return res.status(200).send({ message: "success" });
+   });
+});
+
+app.post("/api/post-free-job", async (req, res, next) => {
+   console.log("🚀 ~ app.post ~ headers", req.headers);
+   console.log("🚀 ~ app.post ~ user-agent", req.headers["user-agent"]);
+
+   const form = formidable();
+   const companyCollectionId = "6299d6392663856bc2f0568a";
+   const jobPostCollectionId = "6299d63926638511b6f05676";
+
+   form.parse(req, async (err, fields, files) => {
+      if (err) {
+         next(err);
+         return;
+      }
+      console.log("🚀 ~ form.parse ~ fields:", fields);
+
+      if (!files?.logoFile?.length || !fields.timeZone?.length) {
+         return res.status(400).send({ message: "Enter all required fields" });
+      }
+      if (files?.logoFile?.length && files.logoFile[0]?.size > MAX_VERCEL_FILE_SIZE_IN_REQUEST) {
+         return res.status(413).send({ message: "Max file size error" });
+      }
+      const fullName = (fields?.fullName && fields.fullName[0]) || "Candidate";
+      const email = fields?.email && fields.email[0];
+      const logoFile = files.logoFile[0];
+      const timeZone = fields.timeZone[0];
+
+      const jobTitle = (fields?.jobTitle && fields.jobTitle[0]) || "N/A";
+      const jobCategory = (fields?.jobCategory && fields.jobCategory[0]) || "N/A";
+      const jobType = fields?.jobType && fields.jobType[0];
+      const jobSalary = (fields?.jobSalary && fields.jobSalary[0]) || "";
+      const showSalary = fields?.showSalary && fields.showSalary[0];
+      const jobDescription = fields?.jobDescription && fields.jobDescription[0];
+      const companyName = (fields?.companyName && fields.companyName[0]) || "N/A";
+      const companyWebsite = fields?.companyWebsite && fields.companyWebsite[0];
+      const linkedinPage = fields?.linkedinPage && fields.linkedinPage[0];
+      const companyDescription = fields?.companyDescription && fields.companyDescription[0];
+      const companyPerksAndBenefits =
+         fields?.companyPerksAndBenefits && fields.companyPerksAndBenefits[0];
+      const remote = fields?.remote && fields.remote[0];
+
+      let fileFormat = getUrlExtension(logoFile.originalFilename);
+
+      let companyCreated;
+      let jobPostCreated;
+      let timeZoneArray: any = [];
+      let salary;
+
+      if (jobSalary.length < 2) {
+         salary = "";
+      } else {
+         salary = `$${jobSalary} USD`;
+      }
+
+      const makeid = (length: any) => {
+         var result = "";
+         var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+         var charactersLength = characters.length;
+         for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+         }
+         return result;
+      };
+
+      const jobTypes: any = {
+         full: "c0dd888f4cd03ba945520c799657f9a7",
+         part: "672d5fa6c321fc864cfd815bddabd4c4",
+      };
+      const remoteStatus: any = {
+         yes: "6340bf99aa99f016c8e83f58f9fd406f",
+         no: "936c1fd466106f1366c0a4cf2000b655",
+      };
+      const category: any = {
+         development: "6299d6392663851fa4f056fe",
+         marketing: "6299d639266385cdb7f05701",
+         support: "6299d6392663857fc1f05703",
+         "business-development": "6304f722907a4ffc0e44d01a",
+         "finance-legal": "6304f731ba0ce449f72c632d",
+         management: "6304f4eb3c9402eca690ad0c",
+         "hr-recruiting": "6307a1345394f2ca1fba72e0",
+         "design-creative": "6304f48b7c43212ff1a68e20",
+      };
+      const timeZones: any = {
+         "anywhere-in-the-world": "6299d639266385006ef056fa",
+         usa: "6299d6392663854931f056f9",
+         europe: "6299d6392663857904f056f8",
+         сanada: "6299d63926638539dbf056f7",
+         asia: "62b08c93e78f9ce631a77bc5",
+         australia: "62b08cacd1cd09c652f2db8d",
+         "latin-america": "62b08cbd1036bfbceecbf614",
+         "middle-east": "62c5b8afd59dce0af20caeaf",
+      };
+      const webflowRadioData: any = {
+         jobType: null,
+         remote: null,
+      };
+      function parseJobDataFromWF() {
+         if (jobType === "part-time") {
+            webflowRadioData.jobType = jobTypes.part;
+         } else if (jobType === "full-time") {
+            webflowRadioData.jobType = jobTypes.full;
+         }
+
+         if (remote === "yes") {
+            webflowRadioData.remote = remoteStatus.yes;
+         } else if (remote === "no") {
+            webflowRadioData.remote = remoteStatus.no;
+         }
+      }
+
+      function updateTimeZoneArray() {
+         if (timeZone.includes("Time-Zone-Anywhere-In-The-World")) {
+            timeZoneArray = [...timeZoneArray, timeZones["anywhere-in-the-world"]];
+         }
+         if (timeZone.includes("Time-Zone-Usa")) {
+            timeZoneArray = [...timeZoneArray, timeZones["usa"]];
+         }
+         if (timeZone.includes("Time-Zone-Europe")) {
+            timeZoneArray = [...timeZoneArray, timeZones["europe"]];
+         }
+         if (timeZone.includes("Time-Zone-Canada")) {
+            timeZoneArray = [...timeZoneArray, timeZones["сanada"]];
+         }
+         if (timeZone.includes("Time-Zone-Asia")) {
+            timeZoneArray = [...timeZoneArray, timeZones["asia"]];
+         }
+         if (timeZone.includes("Time-Zone-Australia")) {
+            timeZoneArray = [...timeZoneArray, timeZones["australia"]];
+         }
+         if (timeZone.includes("Time-Zone-Latin-America")) {
+            timeZoneArray = [...timeZoneArray, timeZones["latin-america"]];
+         }
+         if (timeZone.includes("Time-Zone-Middle-East")) {
+            timeZoneArray = [...timeZoneArray, timeZones["middle-east"]];
+         }
+      }
+
+      let slug = companyName
+         .toLowerCase()
+         .replace(/ /g, "-")
+         .replace(/[^\w-]+/g, "");
+      const rndInt = Math.floor(Math.random() * 100) + 100;
+      let uniqeID = makeid(21);
+
+      const s3FileUrl = await uploadFileToWebflow(logoFile);
+
+      const companyResponse = await axios.post(
+         `https://api.webflow.com/v2/collections/${companyCollectionId}/items`,
+         {
+            isArchived: false,
+            isDraft: true,
+            fieldData: {
+               slug: `${rndInt}-${slug}`,
+               name: companyName,
+               "company-website-link": companyWebsite,
+               "company-linkedin-link": linkedinPage,
+               "company-about": companyDescription,
+               "company-logo-isotype": {
+                  fileId: uniqeID,
+                  url: s3FileUrl,
+                  alt: null,
+               },
+               "company-perks-benefits-left-column": companyPerksAndBenefits,
+               moderated: false,
+            },
+         },
+         {
+            headers: {
+               accept: "application/json",
+               "content-type": "application/json",
+               authorization: `Bearer ${process.env.WEBHOOK_API_TOKEN}`,
+            },
+         }
+      );
+      companyCreated = companyResponse.data;
+
+      // get company id
+      const companyId = companyCreated.id;
+      console.log(`Got here: companyData`, companyId);
+
+      // if collection data exist - create webflow cms item (job post)
+
+      // get company name and job title to convert it intro slug
+      const slugCompany = companyName
+         .toLowerCase()
+         .replace(/ /g, "-")
+         .replace(/[^\w-]+/g, "");
+
+      const slugJobPost = jobTitle
+         .toLowerCase()
+         .replace(/ /g, "-")
+         .replace(/[^\w-]+/g, "");
+
+      // call all nedded functions
+      parseJobDataFromWF();
+      updateTimeZoneArray();
+      //create job post
+
+      const jobPostResponse = await axios.post(
+         `https://api.webflow.com/v2/collections/${jobPostCollectionId}/items`,
+         {
+            isArchived: false,
+            isDraft: true,
+            fieldData: {
+               slug: `${slugCompany}-${slugJobPost}`,
+               name: jobTitle,
+               remote: webflowRadioData.remote,
+               "job-type": webflowRadioData.jobType,
+               "job-description": jobDescription,
+               "job-salary": salary,
+               "job-company": companyId,
+               "job-department": category[jobCategory],
+               "job-application-link": companyWebsite,
+               "show-salary": showSalary,
+               "time-zone-2": timeZoneArray,
+               "job-post-owner-email": email,
+               moderated: false,
+            },
+         },
+         {
+            headers: {
+               accept: "application/json",
+               "content-type": "application/json",
+               authorization: `Bearer ${process.env.WEBHOOK_API_TOKEN}`,
+            },
+         }
+      );
+      jobPostCreated = jobPostResponse.data;
+
+      console.log(`companyCreated`, companyCreated);
+      console.log(`jobPostCreated`, jobPostCreated);
+
+      const tableString = createTableString({
+         fullName,
+         email,
+         jobTitle,
+         remote,
+         jobCategory,
+         jobType,
+         timeZone,
+         jobSalary,
+         showSalary,
+         jobDescription,
+         companyName,
+         companyWebsite,
+         linkedinPage,
+         companyDescription,
+         companyPerksAndBenefits,
+         logo: s3FileUrl,
+      });
+
+      await sendEmail(tableString, "Job post created");
 
       return res.status(200).send({ message: "success" });
    });
